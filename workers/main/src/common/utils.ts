d3968ac4ddbeca8ef9e1ee23f3405a46d37cbbba
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2025-05-28 14:21:59 +0200

    Fix error message variable in environment validation utility
    This commit updates the variable name from `message` to `errorMessage` in the `validateEnv` function within the `utils.ts` file. This change improves clarity and consistency in error logging for missing or invalid environment variables, ensuring that the correct variable is referenced when logging errors. The overall functionality of the environment validation remains unchanged.

--- workers/main/src/common/utils.ts
+++ workers/main/src/common/utils.ts
@@ -2,14 +2,14 @@ import { validationResult } from '../configs';
 
 export function validateEnv() {
   if (!validationResult.success) {
-    const message = validationResult.error.issues
+    const errorMessage = validationResult.error.issues
       .map(
         ({ path, message }) =>
           `Missing or invalid environment variable: ${path.join('.') || '(unknown variable)'} (${message})`,
       )
       .join('\n');
 
-    console.error(message);
+    console.error(errorMessage);
     process.exit(1);
   }
 }
