804c270df8ceb5da56d84a6d4af49a4cb61d7d18
anatolyshipitz <30830673+anatolyshipitz@users.noreply.github.com>
2025-06-08 19:43:26 +0200

    Add getFinAppData functionality and corresponding tests (#53)
    - Introduced `getFinAppData` function to fetch financial application
    data from a specified JSON file, process it, and save the results to a
    new file.
    - Implemented error handling using `AppError` for various failure
    scenarios, ensuring robust error reporting.
    - Added unit tests for `getFinAppData` to validate successful data
    retrieval and error handling, including mock implementations for
    dependencies.
    - Updated the `index.ts` file to export the new `getFinAppData` function
    for broader access.
    
    These changes enhance the financial reporting capabilities and improve
    test coverage for data processing operations.

--- workers/main/src/workflows/weeklyFinancialReports/weeklyFinancialReports.workflow.ts
+++ workers/main/src/workflows/weeklyFinancialReports/weeklyFinancialReports.workflow.ts
@@ -2,12 +2,13 @@ import { proxyActivities } from '@temporalio/workflow';
 
 import type * as activities from '../../activities/weeklyFinancialReports';
 
-const { getTargetUnits } = proxyActivities<typeof activities>({
+const { getTargetUnits, getFinAppData } = proxyActivities<typeof activities>({
   startToCloseTimeout: '10 minutes',
 });
 
 export async function weeklyFinancialReportsWorkflow(): Promise<string> {
   const targetUnits = await getTargetUnits();
+  const finData = await getFinAppData(targetUnits.fileLink);
 
-  return targetUnits.fileLink;
+  return finData.fileLink;
 }
