52a2dd7081ceec8ac6b61f79e5b14d935b8bebed
anatolyshipitz <30830673+anatolyshipitz@users.noreply.github.com>
2025-06-11 12:13:03 +0200

    Enhance weekly financial reports workflow with group name validation (#60)
    - Updated `weeklyFinancialReportsWorkflow` to accept a `groupName`
    parameter, ensuring it is validated against the `GroupNameEnum`.
    - Added error handling to throw an `AppError` for invalid group names,
    improving robustness and clarity in error reporting.
    - Modified the corresponding test to reflect the new parameter usage.
    
    These changes enhance the workflow's functionality and ensure that only
    valid group names are processed.

--- workers/main/src/workflows/weeklyFinancialReports/weeklyFinancialReports.workflow.ts
+++ workers/main/src/workflows/weeklyFinancialReports/weeklyFinancialReports.workflow.ts
@@ -1,6 +1,9 @@
 import { proxyActivities } from '@temporalio/workflow';
 
 import type * as activities from '../../activities/weeklyFinancialReports';
+import { AppError } from '../../common/errors';
+import { GroupName } from '../../common/types';
+import { GroupNameEnum } from '../../configs/weeklyFinancialReport';
 
 const { getTargetUnits, fetchFinancialAppData } = proxyActivities<
   typeof activities
@@ -8,7 +11,15 @@ const { getTargetUnits, fetchFinancialAppData } = proxyActivities<
   startToCloseTimeout: '10 minutes',
 });
 
-export async function weeklyFinancialReportsWorkflow(): Promise<string> {
+export async function weeklyFinancialReportsWorkflow(
+  groupName: GroupName,
+): Promise<string> {
+  if (!(Object.values(GroupNameEnum) as GroupName[]).includes(groupName)) {
+    throw new AppError(
+      `Invalid groupName paramter: ${groupName}. Allowed values: "${Object.values(GroupNameEnum).join('", "')}"`,
+      'weeklyFinancialReportsWorkflow',
+    );
+  }
   const targetUnits = await getTargetUnits();
   const finData = await fetchFinancialAppData(targetUnits.fileLink);
 
