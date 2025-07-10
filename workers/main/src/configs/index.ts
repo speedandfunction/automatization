16f76f3bacba2b036c3c74168964e38292e57dc7
anatoly.shipitz <anatoly.shipitz@speedandfunction.com>
2025-07-10 17:24:33 +0200

    feat(config): Add QBO configuration and schema validation
    - Introduced a new `qbo.ts` file containing the QBO API configuration and schema validation using Zod.
    - Updated the main configuration file to include the QBO schema in the validation process.
    
    These changes enhance the application's configuration management by integrating QBO API settings, ensuring proper validation of required environment variables.

--- workers/main/src/configs/index.ts
+++ workers/main/src/configs/index.ts
@@ -3,10 +3,12 @@ import { redmineDatabaseSchema } from './redmineDatabase';
 import { slackSchema } from './slack';
 import { temporalSchema } from './temporal';
 import { workerSchema } from './worker';
+import { qboSchema } from './qbo';
 
 export const validationResult = temporalSchema
   .merge(workerSchema)
   .merge(slackSchema)
   .merge(redmineDatabaseSchema)
   .merge(mongoDatabaseSchema)
+  .merge(qboSchema)
   .safeParse(process.env);
---
+++ workers/main/src/configs/qbo.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+export const qboConfig = {
+  apiUrl: process.env.QBO_API_URL,
+  bearerToken: process.env.QBO_BEARER_TOKEN,
+};
+
+export const qboSchema = z.object({
+  QBO_API_URL: z.string().url(),
+  QBO_BEARER_TOKEN: z.string(),
+}); 
